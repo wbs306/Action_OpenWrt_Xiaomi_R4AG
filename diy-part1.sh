63fe21b5dca1c3e40fdf080167085a851517ae2a
ababwbq <82033636+ababwbq@users.noreply.github.com>
2022-04-18 23:01:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -48,23 +48,3 @@ export imsize1=$(grep  -a -n -e 'define Device/xiaomi_mir3g-v2' target/linux/ram
 export imsize1=$(expr $imsize1 + 2)
 export imsize1=$(echo $imsize1"s")
 sed -i "$imsize1/IMAGE_SIZE := .*/IMAGE_SIZE := 16064k/" target/linux/ramips/image/mt7621.mk
-
-### 修改为R4A百兆版Breed直刷版
-## 1.修改 mt7628an_xiaomi_mi-router-4a-100m.dts
-export shanchu1=$(grep  -a -n -e '&spi0 {' target/linux/ramips/dts/mt7628an_xiaomi_mi-router-4a-100m.dts|cut -d ":" -f 1)
-export shanchu2=$(grep  -a -n -e '&pcie {' target/linux/ramips/dts/mt7628an_xiaomi_mi-router-4a-100m.dts|cut -d ":" -f 1)
-export shanchu2=$(expr $shanchu2 - 1)
-export shanchu2=$(echo $shanchu2"d")
-sed -i $shanchu1,$shanchu2 target/linux/ramips/dts/mt7628an_xiaomi_mi-router-4a-100m.dts
-grep  -Pzo '&spi0[\s\S]*};[\s]*};[\s]*};[\s]*};' target/linux/ramips/dts/mt7621_youhua_wr1200js.dts > youhua.txt
-echo "" >> youhua.txt
-echo "" >> youhua.txt
-export shanchu1=$(expr $shanchu1 - 1)
-export shanchu1=$(echo $shanchu1"r")
-sed -i "$shanchu1 youhua.txt" target/linux/ramips/dts/mt7628an_xiaomi_mi-router-4a-100m.dts
-rm -rf youhua.txt
-## 2.修改mt76x8.mk
-export imsize1=$(grep  -a -n -e 'define Device/xiaomi_mi-router-4a-100m' target/linux/ramips/image/mt76x8.mk|cut -d ":" -f 1)
-export imsize1=$(expr $imsize1 + 2)
-export imsize1=$(echo $imsize1"s")
-sed -i "$imsize1/IMAGE_SIZE := .*/IMAGE_SIZE := 16064k/" target/linux/ramips/image/mt76x8.mk
