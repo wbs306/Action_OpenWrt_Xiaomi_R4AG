97c7121d4b208329c3b69d193109292caf477f91
wbs306 <wbs306@gmail.com>
2022-05-10 23:00:55 +0800

    Correct device name

--- diy-part1.sh
+++ diy-part1.sh
@@ -44,7 +44,7 @@ export shanchu1=$(echo $shanchu1"r")
 sed -i "$shanchu1 youhua.txt" target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
 rm -rf youhua.txt
 ## 2.修改mt7621.mk
-export imsize1=$(grep  -a -n -e 'define Device/xiaomi_mir3g-v2' target/linux/ramips/image/mt7621.mk|cut -d ":" -f 1)
+export imsize1=$(grep  -a -n -e 'define Device/xiaomi_mi-router-4a-gigabit' target/linux/ramips/image/mt7621.mk|cut -d ":" -f 1)
 export imsize1=$(expr $imsize1 + 2)
 export imsize1=$(echo $imsize1"s")
 sed -i "$imsize1/IMAGE_SIZE := .*/IMAGE_SIZE := 16064k/" target/linux/ramips/image/mt7621.mk
