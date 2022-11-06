b0f229dd7f018fa95c541190788d5fb22bb8e2e0
Mr.TaiKe <117250841+MrTaiKe@users.noreply.github.com>
2022-11-06 10:35:38 +0800

    clear up

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,22 +9,11 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-## Xiaomi R4AG_V2 Breed Flash 直刷版 ( R4AG V2 HW changed!)
-## Many Thx to Wbs2036's support for Xiaomi R4AG V2 ! wbs306/lede@a2a3a4a commits: 
-## 1.target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-gigabit-v2.dts
-## 2.target/linux/ramips/image/mt7621.mk
-## 3.target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-
-
-### 修改主题文件
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
 # 添加插件源码
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
+### 修改主题文件
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
