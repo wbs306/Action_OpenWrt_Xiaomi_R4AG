625979510fe7ca1539e70d1c427b9a5031e05084
Mr.TaiKe <117250841+MrTaiKe@users.noreply.github.com>
2022-11-07 22:25:43 +0800

    Update diy-part3_mf.sh

--- diy-part3_mf.sh
+++ diy-part3_mf.sh
@@ -11,5 +11,5 @@
 
 # MOVE Copy File
 
-mv MI-R4A/favicon.ico openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/favicon.ico  
-mv MI-R4A/banner openwrt/package/base-files/files/etc/banner
+mv favicon.ico openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/favicon.ico  
+mv banner openwrt/package/base-files/files/etc/banner
