eef1d7f18679bf989b7743ec1fdd70d7658e786e
Mr.TaiKe <117250841+MrTaiKe@users.noreply.github.com>
2022-11-15 14:09:10 +0800

    comment out armygreen

--- built2/light-part2_config.sh
+++ built2/light-part2_config.sh
@@ -41,7 +41,7 @@ sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.defa
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon_armygreen/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 
 #########################################################
