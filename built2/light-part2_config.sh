ad941504a47dc9a3f65f2938c344663609ec75a1
Mr.TaiKe <117250841+MrTaiKe@users.noreply.github.com>
2022-11-15 23:12:31 +0800

    sudo git clone

--- built2/light-part2_config.sh
+++ built2/light-part2_config.sh
@@ -12,11 +12,20 @@
 #
 
 # Add Theme package
-rm -rf openwrt/package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git openwrt/package/lean/luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+sudo git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
+#########################################################
 #rm -rf package/lean/luci-theme-argon_armygreen
-#git clone https://github.com/XXKDB/luci-theme-argon_armygreen.git openwrt/package/lean/luci-theme-argon_armygreen
+#git clone https://github.com/XXKDB/luci-theme-argon_armygreen.git package/lean/luci-theme-argon_armygreen
+
+#登陆页面右下角技术支持  跳转网站“https://github.com/openwrt/luci”   名称“可爱动漫主题”
+#sed -i 's/可爱动漫主题/可爱动漫主题/g' package/lean/luci-theme-argon_armygreen/luasrc/view/themes/argon_armygreen/footer.htm
+
+#主机名右上角符号❤
+#sed -i 's/❤/❤/g' package/lean/luci-theme-argon_armygreen/luasrc/view/themes/argon_armygreen/header.htm
+
+#########################################################
 
 
 # Basic Modification 
@@ -44,11 +53,4 @@ sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 
-#########################################################
-#登陆页面右下角技术支持  跳转网站“https://github.com/openwrt/luci”   名称“可爱动漫主题”
-#sed -i 's/可爱动漫主题/可爱动漫主题/g' package/lean/luci-theme-argon_armygreen/luasrc/view/themes/argon_armygreen/footer.htm
 
-#主机名右上角符号❤
-#sed -i 's/❤/❤/g' package/lean/luci-theme-argon_armygreen/luasrc/view/themes/argon_armygreen/header.htm
-
-#########################################################
