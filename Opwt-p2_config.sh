46a8668c90cecab64b1129ea783449f87aff86ce
Mr.TaiKe <117250841+MrTaiKe@users.noreply.github.com>
2022-11-16 16:44:47 +0800

    Jeffy Theme package

--- Opwt-p2_config.sh
+++ Opwt-p2_config.sh
@@ -2,8 +2,11 @@
 # Description: OpenWrt DIY script part 2 Configuration (After Update feeds)
 
 # Add Theme package
-rm -rf package/luci-theme-argon
-sudo git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+rm -rf package/lean/luci-app-argon-config
+sudo git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+sudo git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+
 
 # Basic Modification 
 
