6697e7237e692639e6757ea78fbcd612329ba79a
Mr.TaiKe <117250841+MrTaiKe@users.noreply.github.com>
2022-11-16 16:40:54 +0800

    kenzo openwrt package

--- Opwt-p1_feeds.sh
+++ Opwt-p1_feeds.sh
@@ -3,4 +3,7 @@
 
 
 # 添加插件源码 Add a feed source
-echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+#echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
