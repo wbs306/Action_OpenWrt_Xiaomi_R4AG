fb3c1f5a3aee6f9505f36e5aadb4806a541e67ee
Mr.TaiKe <117250841+MrTaiKe@users.noreply.github.com>
2022-11-17 01:18:21 +0800

    mini

--- Opwt-p2_config.sh
+++ Opwt-p2_config.sh
@@ -13,12 +13,6 @@
 # 修改openwrt登陆地址,把下面的192.168.31.1修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
-# 修改主机名字，把Xiaomi-R4A修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='R4AGv2'' package/default-settings/files/zzz-default-settings
-
-# 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i "s/OpenWrt /Mr.TaiKe build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
-
 # 修改默认 wifi 名称 ssid为 WiFi_R4AGv2
 sed -i 's/ssid=OpenWrt/ssid=WiFi_R4AGv2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
