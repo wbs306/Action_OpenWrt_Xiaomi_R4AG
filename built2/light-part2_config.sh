46a676e3a7ec541b9ebf1a10161ff18cb8d27879
Mr.TaiKe <117250841+MrTaiKe@users.noreply.github.com>
2022-11-16 14:34:21 +0800

    Argon theme 19.07

--- built2/light-part2_config.sh
+++ built2/light-part2_config.sh
@@ -13,7 +13,7 @@
 
 # Add Theme package
 rm -rf package/lean/luci-theme-argon
-sudo git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+sudo git clone -b 19.07 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 #########################################################
 #rm -rf package/lean/luci-theme-argon_armygreen
