37650f5595aaf65ec8e67a32a79fe080959ee1fd
Mr.TaiKe <117250841+MrTaiKe@users.noreply.github.com>
2022-11-18 21:06:51 +0800

    fix and update feeds

--- Opwt-p1_feeds.sh
+++ Opwt-p1_feeds.sh
@@ -12,9 +12,3 @@ rm -rf package/luci-theme-argon
 rm -rf package/luci-app-argon-config
 sudo git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 sudo git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-
-#取消原主题luci-theme-bootstrap为默认主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
-# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
